581fbb018d5a2880bf5133bea2f17dd7e933e24c
mroreoman <830aryans@gmail.com>
2025-03-05 14:38:45 -0500

    cleaning & added win message

--- watersortpuzzle.py
+++ watersortpuzzle.py
@@ -2,6 +2,10 @@ from enum import Enum
 import random
 import tkinter as tk
 
+NUM_TUBES = 4
+if NUM_TUBES > 9:
+    NUM_TUBES = 9
+
 class Liquid(Enum):
     RED = "R"
     ORANGE = "O"
@@ -68,6 +72,9 @@ class Tube:
         return s
 
 def pour(giving: Tube, receiving: Tube):
+    print(f"{giving} -> {receiving}")
+    if giving is receiving:
+        return
     while giving.top() and (giving.top() == receiving.top() or not receiving.top()):
         if receiving.space():
             receiving.push(giving.pop())
@@ -77,6 +84,7 @@ def pour(giving: Tube, receiving: Tube):
 def minipour(giving: Tube, receiving: Tube):
     if giving.top() and receiving.space():
         receiving.push(giving.pop())
+        print(f"{giving} -> {receiving}")
 
 tubes:list[Tube] = []
 selected_tube:Tube = None
@@ -95,18 +103,22 @@ def solved():
 
 def select(tube:Tube):
     global selected_tube
-    print("selected " + str(tube))
+    # print("selected " + str(tube))
     tube.button.config(relief=tk.SUNKEN)
     selected_tube = tube
 
 def deselect():
     global selected_tube
-    print("deselected " + str(selected_tube))
+    # print("deselected " + str(selected_tube))
     selected_tube.button.config(relief=tk.RAISED)
     selected_tube = None
 
+def finish_board():
+    print("board solved")
+    message.config(text="you won!")
+
 def click(tube:Tube):
-    print(f"clicked {tube}")
+    # print(f"clicked {tube}")
     if not selected_tube:
         if tube.top(): # tube isn't empty
             select(tube)
@@ -115,7 +127,7 @@ def click(tube:Tube):
             pour(selected_tube, tube)
             deselect()
             if solved():
-                print("board solved")
+                finish_board()
         else:
             deselect()
             select(tube)
@@ -124,27 +136,32 @@ def clear():
     for tube in tubes:
         tube.frame.destroy()
     tubes.clear()
+    message.config(text="")
 
 def init():
     clear()
-    print(tubes)
-
-    for liquid in Liquid:
-        tube = Tube([liquid,]*Tube.max)
-        tubes.append(tube)
-    for _ in range(1):
+    liquids = list(Liquid)
+    if NUM_TUBES <= 4:
+        for _ in range(NUM_TUBES-1):
+            tubes.append(Tube([liquids.pop(random.randint(0, len(liquids)-1)),]*Tube.max))
         tubes.append(Tube([]))
+    else:
+        for _ in range(NUM_TUBES-2):
+            tubes.append(Tube([liquids.pop(random.randint(0, len(liquids)-1)),]*Tube.max))
+        for _ in range(2):
+            tubes.append(Tube([]))
     
     for tube in tubes:
         tube.button.config(command = (lambda x: (lambda: click(x)))(tube))
     
-    for _ in range(100):
+    for _ in range(20):
         minipour(random.choice(tubes), random.choice(tubes))
     
     print_tubes()
 
 root = tk.Tk()
 root.title("water sort")
+root.minsize(400,200)
 root.tk.call('tk', 'scaling', 2.5)
 
 main = tk.Frame()
@@ -152,5 +169,8 @@ main.pack(padx=3, pady=3)
 
 start = tk.Button(main, text="new game", command=init)
 start.pack(side = tk.BOTTOM)
+
+message = tk.Label(main, text="welcome!")
+message.pack(side = tk.BOTTOM)
 
 root.mainloop()
